98929dc010ed13ba01b0ae297f05df3270d0d267
azegha <loicanicetazegha@gmail.com>
2024-01-18 15:46:11 +0100

    test

--- src/main/java/com/example/projecthibernatecrud/entity/InstructorDetail.java
+++ src/main/java/com/example/projecthibernatecrud/entity/InstructorDetail.java
@@ -1,4 +1,50 @@
 package com.example.projecthibernatecrud.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "instructor_detail")
 public class InstructorDetail {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "id")
+private int id;
+@Column(name = "hobby")
+private String hobby;
+@Column(name = "youtube_channel")
+private String youtubeChannel;
+
+public InstructorDetail(){
+
+}
+
+    public InstructorDetail(String hobby, String youtubeChannel) {
+        this.hobby = hobby;
+        this.youtubeChannel = youtubeChannel;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
 }
